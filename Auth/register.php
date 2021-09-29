f712bcf171bb6e009b0ed254ac99cd5b09555d08
LARGE IRON ORE VEIN <newWorld@gmail.com>
2021-09-29 17:28:18 +0200

    register

---
+++ Auth/register.php
@@ -0,0 +1,24 @@
+<?php
+include 'Models/DatabaseDriver.php';
+//dzadaz
+if (isset($_POST['reg_user'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $errors = array();
+    if (empty($username)) { array_push($errors, "Username is required"); }
+    if (empty($email)) { array_push($errors, "Email is required"); }
+    if (empty($password)) { array_push($errors, "Password is required"); }
+
+    if(empty($errors)){
+        $dbd = new DatabaseDriver;
+        $code_err = $dbd->insertUser($username,password_hash($password,PASSWORD_BCRYPT),$email);
+        if($code_err ==-1){
+            array_push($errors, "Failed to register user");
+            return $errors;
+        }else{
+            return "User sucessfully registered , please log in";
+        }
+    }
+
+?>
