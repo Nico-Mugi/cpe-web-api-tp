29094a78dccd5d12ca173ce3ab97885d147be2f4
Eloi Bernard <eloi.bernard@coexya.eu>
2021-10-13 15:26:17 +0200

    clean router

--- router.php
+++ router.php
@@ -24,56 +24,55 @@ if ($method != 'GET') {
 }
 
 // traiter requête
-switch ($request[1]) {
-    case 'meridiens':
-        // routes : /meridiens/all
-        if ($request[2] != null && $request[2] == 'all') {
-            $data = $dbd->getAllMeridien();
-            header('Content-Type: application/json');
-            echo json_encode($data);
-        } else {
-            echo ('error : not supported');
-            http_response_code(400);
-            exit();
-        }
-        break;
-    case 'symptomes':
-        // routes : /symptomes/all
-        if ($request[2] != null && $request[2] == 'all') {
-            $data = $dbd->getAllSymptomes();
-            header('Content-Type: application/json');
-            echo json_encode($data);
-        } else {
-            echo ('error : not supported');
-            http_response_code(400);
-            exit();
-        }
-        break;
-    case 'pathologies':
-        // routes : /pathologies/all
-        //          /pathologies/byKeyword/:keyword
-        if ($request[2] != null) {
-            if ($request[2] == 'all') {
-                $data = $dbd->getAllPatho();
+if (array_key_exists(1, $request)) {
+    switch ($request[1]) {
+        case 'meridiens':
+            // routes : /meridiens/all
+            if (array_key_exists(2, $request) && $request[2] == 'all') {
+                $data = $dbd->getAllMeridien();
                 header('Content-Type: application/json');
                 echo json_encode($data);
-            } elseif ($request[2] == 'byKeyword' && $request[3] != null) {
-                $data = $dbd->getPathosByKeyWord($request[3]);
-                header('Content-Type: application/json');
-                echo json_encode($data);
+                break;
             }
-        } else {
-            echo ('error : not supported');
             http_response_code(400);
             exit();
-        }
-        break;
+        case 'symptomes':
+            // routes : /symptomes/all
+            if (array_key_exists(2, $request) && $request[2] == 'all') {
+                $data = $dbd->getAllSymptomes();
+                header('Content-Type: application/json');
+                echo json_encode($data);
+                break;
+            }
+            http_response_code(400);
+            exit();
 
-    default:
-        echo ('error : ressource not found');
-        http_response_code(404);
-        exit();
-        break;
+        case 'pathologies':
+            // routes : /pathologies/all
+            //          /pathologies/byKeyword/:keyword
+            if (array_key_exists(2, $request)) {
+                if ($request[2] == 'all') {
+                    $data = $dbd->getAllPatho();
+                    header('Content-Type: application/json');
+                    echo json_encode($data);
+                    break;
+                } elseif ($request[2] == 'byKeyword' && array_key_exists(3, $request)) {
+                    $data = $dbd->getPathosByKeyWord($request[3]);
+                    header('Content-Type: application/json');
+                    echo json_encode($data);
+                    break;
+                }
+            }
+            http_response_code(400);
+            exit();
+
+        default:
+            http_response_code(404);
+            exit();
+    }
+} else {
+    http_response_code(400);
+    exit();
 }
 
 http_response_code(200);
